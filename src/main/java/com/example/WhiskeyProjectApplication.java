4c4c1364f521f3545cfe4d0f189198a856d95c33
nooum39 <mikantaruto1380@gmail.com>
2024-05-22 13:13:14 +0900

    プロジェクト作成

---
+++ src/main/java/com/example/WhiskeyProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhiskeyProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhiskeyProjectApplication.class, args);
+	}
+
+}
